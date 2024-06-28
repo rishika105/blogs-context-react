f849d256a56337b89de05f340717dd678a7fc825
Rishika Agarwal <rishikaagarwal2316@gmail.com>
2024-06-28 10:15:25 +0530

    debugged

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useContext, useEffect } from "react";
-import { AppContext } from "./Context/AppContext";
+import { AppContext } from "./context/AppContext";
 import Home from "./Pages/Home";
 import TagPage from "./Pages/TagPage";
 import CategoryPage from "./Pages/CategoryPage";
@@ -9,31 +9,33 @@ import { Routes, Route, useSearchParams, useLocation } from "react-router-dom";
 
 export default function App() {
   const { fetchBlogPosts } = useContext(AppContext);
-  const [searchParams] = useSearchParams();
+  const [searchParams,setSearchParams] = useSearchParams();
   const location = useLocation();
 
   useEffect(() => {
-    // Fetch the initial Blogposts data
-    fetchBlogPosts();
+    // // Fetch the inital Blogposts data
+    // fetchBlogPosts();
+    // // eslint-disable-next-line react-hooks/exhaustive-deps
 
     const page = searchParams.get('page') ?? 1;
-
-    if (location.pathname.includes("tags")) {
+    if(location.pathname.includes("tags")){
       // Tag Page 
-      const tag = location.pathname.split("/").at(-1).replaceAll("-", " ");
-      fetchBlogPosts(Number(page), tag);
-    } else if (location.pathname.includes("categories")) {
-      const category = location.pathname.split("/").at(-1).replaceAll("-", " ");
-      fetchBlogPosts(Number(page), null, category);
-    } else {
-      fetchBlogPosts(Number(page));
+      const tag = location.pathname.split("/").at(-1).replaceAll("-"," ");
+      fetchBlogPosts(Number(page),tag);
     }
-  }, [fetchBlogPosts, location.pathname, location.search, searchParams]);
+    else if(location.pathname.includes("categories")){
+      const category = location.pathname.split("/").at(-1).replaceAll("-"," ");
+      fetchBlogPosts(Number(page),null,category);
+    }
+    else{
+      fetchBlogPosts(Number(page))
+    }
+  }, [location.pathname,location.search]);
 
   return (
     <Routes>
-      <Route path="/*" element={<Home />} />
-      <Route path="/blog/:blogId" element={<BlogPage />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/blog/:blogId" element={<BlogPage /> } />
       <Route path="/tags/:tag" element={<TagPage />} />
       <Route path="/categories/:category" element={<CategoryPage />} />
     </Routes>
--- src/Pages/BlogPage.js
+++ src/Pages/BlogPage.js
@@ -1,50 +1,45 @@
 import React, { useContext, useEffect, useState, useCallback } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { AppContext } from "../Context/AppContext";
+import { AppContext } from "./Context/AppContext";
 import Header from "../Components/Header";
 import BlogDetails from "../Components/BlogDetails";
 
 const BlogPage = () => {
   const [blog, setBlog] = useState(null);
-  const [relatedBlogs, setRelatedBlogs] = useState([]);
+  const [relatedblog, setRelatedblog] = useState([]);
   const location = useLocation();
-  const navigate = useNavigate();
+  const navigation = useNavigate();
   const { loading, setLoading } = useContext(AppContext);
   const blogId = location.pathname.split("/").at(-1);
   const newBaseUrl = "https://codehelp-apis.vercel.app/api/";
 
-  const fetchRelatedBlogs = useCallback(async () => {
-    if (!blogId) return;
-
+  async function fetchRelatedBlogs() {
     setLoading(true);
     let url = `${newBaseUrl}get-blog?blogId=${blogId}`;
     try {
       const res = await fetch(url);
       const data = await res.json();
       setBlog(data.blog);
-      setRelatedBlogs(data.relatedBlogs);
+      setRelatedblog(data.relatedBlogs);
     } catch (err) {
       console.log(err);
       setBlog(null);
-      setRelatedBlogs([]);
+      setRelatedblog([]);
     }
     setLoading(false);
-  }, [blogId, newBaseUrl, setLoading]);
+  }
 
   useEffect(() => {
-    fetchRelatedBlogs();
-  }, [fetchRelatedBlogs]);
+    if (blogId) {
+      fetchRelatedBlogs();
+    }
+  }, [location.pathname]);
 
   return (
-    <div className="max-w-[620px] w-11/12 py-3 flex flex-col gap-y-7 my-[100px] mx-auto">
+    <div>
       <Header />
       <div>
-        <button
-          className="border-2 border-gray-300 py-1 px-4 rounded-md"
-          onClick={() => navigate(-1)}
-        >
-          Back
-        </button>
+        <button onClick={() => navigation(-1)}>Back</button>
       </div>
       <div>
         {loading ? (
@@ -52,8 +47,8 @@ const BlogPage = () => {
         ) : blog ? (
           <div>
             <BlogDetails post={blog} />
-            <h2 className="font-bold text-2xl mt-5 mb-3">Related Blogs</h2>
-            {relatedBlogs.map((post) => (
+            <h2>Related Blogs</h2>
+            {relatedblog.map((post) => (
               <div key={post.id}>
                 <BlogDetails post={post} />
               </div>
@@ -66,5 +61,4 @@ const BlogPage = () => {
     </div>
   );
 };
-
 export default BlogPage;
